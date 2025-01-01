50072f4df77400100e94315c0c7ea995f2521bf2
Shani <shanidevkashyap8913@gmail.com>
2025-01-01 14:07:26 +0530

    Experience section created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import About from './Components/About'
 import Technology from './Components/Technology'
+import Experience from './Components/Experience'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Hero />
           <About />
           <Technology />
+          <Experience />
         </div>
       </div>
     </div>
