db657b619b6fff746aab31d0751cd27a9b2496c7
Shani <shanidevkashyap8913@gmail.com>
2025-01-02 17:38:00 +0530

    some ui changes in contact-form.jsx

--- src/Components/Contactform.jsx
+++ src/Components/Contactform.jsx
@@ -29,9 +29,9 @@ const Contactform = () => {
     }
   };
   return (
-    <section className="h-screen flex flex-col justify-center items-center border text-white absolute top-0 z-[-2]  w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-      <form onSubmit = {onSubmit} className="absolute flex flex-col items-start gap-4   m-2 p-4">
-        <h2 className=" text-4xl translate-x-36 tracking-tighter pb-20 ">
+    <section className=" container h-screen flex flex-col  items-center text-white absolute top-0 z-[-2]  w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+      <form onSubmit = {onSubmit} className=" absolute flex flex-col items-start gap-4   m-2 p-4">
+        <h2 className=" text-4xl tracking-tighter pb-20 ">
           Contact Form
         </h2>
         <div className="flex  flex-wrap text-2xl p-2 items-center gap-2">
@@ -44,6 +44,7 @@ const Contactform = () => {
             required
           />
         </div>
+        
         <div className="flex  flex-wrap text-2xl p-2 items-center gap-2">
           <label className="px-2">Email Address</label>
           <input
@@ -54,20 +55,22 @@ const Contactform = () => {
             required
           />
         </div>
+        
         <div className="flex  flex-wrap text-2xl p-2 items-center gap-2">
           <label className="px-2">Your Message</label>
+          
           <textarea
-            className="w-96 h-48 rounded m-1 px-2 py-1 text-neutral-900 placeholder:text-lg placeholder:text-slate-300 placeholder:font-semibold  overflow-y-auto"
-            
-            id=""
+            className="rounded m-2 p-2 w-2/3 lg:w-full text-neutral-900 placeholder:text-lg placeholder:text-slate-300 placeholder:font-semibold  overflow-y-auto"
             placeholder="Type your message here"
             name = "message"
             required
           ></textarea>
+         
         </div>
-        <button type="submit" className="border-2 translate-x-40 relative flex m-2 font-semibold p-3 rounded-lg bg-blue-600 border-blue-700  hover:bg-blue-500 ">
+        
+        <button type="submit" className="border-2 relative flex m-2 font-semibold p-3 rounded-lg bg-blue-600 border-blue-700  hover:bg-blue-500 ">
           Send Message
-        </button>
+        </button> 
       </form>
     </section>
   );
